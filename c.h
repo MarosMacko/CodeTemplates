dc095a8d50e0be07a0f501b1280a329c5578b08d
MarosM <mackomarosh@gmail.com>
2019-08-07 13:07:33 +0200

    Create c.h

---
+++ c.h
@@ -0,0 +1,14 @@
+
+// Nice terminal defines using escape codes. 
+// Didn't work properly on Windows when I tried it. Maybe in the new Windows terminal ??
+#define T_CLEAR     "\033[0m"
+#define T_CLS       "\033[2J"
+#define T_BOLD      "\033[1m"
+#define T_UNDERLINE "\033[4m"
+#define T_BLINK     "\033[5m"
+#define T_SWAPBG    "\033[7m"
+#define T_GREEN     "\033[32;1;40m"
+#define T_CYAN      "\033[36;1;40m"
+#define T_MAGENTA   "\033[35;1;40m"
+#define T_YELLOW    "\033[33;1;40m"
+#define T_RED       "\033[31;1;40m"
